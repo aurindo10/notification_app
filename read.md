6aeb33b077d6b5faf8bbffed2a2ef9e03f9f4cd1
Aurindo Neto <aurindo15@gmail.com>
2024-05-21 10:59:17 -0300

    Make the test to register cliente

--- read.md
+++ read.md
@@ -2,6 +2,7 @@
 [x] Adicionar validação dos campos obrigatórios
 [x] Verificar hash
 [x] Verificar se há outro email igual
-[] Make the test to register cliente
-[] Fechar conexão no test main antes de dropar a tabela
-[] Inserir uma nova instancia do bd dentro de clear db
+[x] Make the test to register cliente
+[x] Fechar conexão no test main antes de dropar a tabela
+[x] Inserir uma nova instancia do bd dentro de clear db
+[] Fazer test de cadastro de usuário duplicado
--- user-service/internal/handlers/tests/register_test.go
+++ user-service/internal/handlers/tests/register_test.go
@@ -31,3 +31,52 @@ func TestRegisterUser(t *testing.T) {
 		t.Fatalf("Erro ao decodificar: %v", err)
 	}
 }
+func TestProtectionAgainstEmailDuplicated(t *testing.T) {
+	clientData := map[string]string{
+		"username":  "user1s23",
+		"password":  "passwords123",
+		"name":      "Johna",
+		"last_name": "Dsoe",
+		"email":     "johndaosea@example.com",
+	}
+	clientDataJson, error := json.Marshal(clientData)
+	if error != nil {
+		t.Fatalf("Erro ao converter dados do cliente para JSON: %v", error)
+	}
+	resp, err := http.Post("http://localhost:3000/api/v1/registeruser", "application/json", bytes.NewBuffer(clientDataJson))
+	if err != nil {
+		t.Fatalf("Erro ao fazer solicitação POST: %v", err)
+	}
+	_, err = http.Post("http://localhost:3000/api/v1/registeruser", "application/json", bytes.NewBuffer(clientDataJson))
+	if err != nil {
+		t.Fatalf("Erro ao fazer solicitação POST: %v", err)
+	}
+	defer resp.Body.Close()
+	var decodedResponse string
+	if err := json.NewDecoder(resp.Body).Decode(&decodedResponse); err != nil {
+		t.Fatalf("Erro ao decodificar: %v", err)
+	}
+	if decodedResponse != "email já existe" {
+		t.Fatalf("Teste falhou")
+	}
+}
+func TestEmpitFieldRegister(t *testing.T) {
+	clientData := map[string]string{
+		"username":  "user1s23",
+		"password":  "passwords123",
+		"name":      "Johna",
+		"last_name": "Dsoe",
+	}
+	clientDataJson, error := json.Marshal(clientData)
+	if error != nil {
+		t.Fatalf("Erro ao converter dados do cliente para JSON: %v", error)
+	}
+	resp, err := http.Post("http://localhost:3000/api/v1/registeruser", "application/json", bytes.NewBuffer(clientDataJson))
+	if err != nil {
+		t.Fatalf("Erro ao fazer solicitação POST: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.Status != "400 Bad Request" {
+		t.Fatalf("Teste falhou")
+	}
+}
